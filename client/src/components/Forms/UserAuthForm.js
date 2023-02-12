c4a3ec12967e9aba6855f79ac083c2a973e2de53
Minh Nguyen <vn.ndminh@gmail.com>
2023-02-11 21:59:29 -0600

    Setup react

---
+++ client/src/components/Forms/UserAuthForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const UserAuthForm = () => {
+  return (
+    <div>UserAuthForm</div>
+  )
+}
+
+export default UserAuthForm
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserAuthForm from '../../components/Forms/UserAuthForm'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
