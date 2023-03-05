fb879c71e70529a8f8ef0e1108c35854ecc33248
Quang Du <qdtheta@gmail.com>
2023-03-04 23:42:53 -0600

    PriceModule Unit Test Completed

--- server/test/PriceModule.test.js
+++ server/test/PriceModule.test.js
@@ -1,6 +1,6 @@
 const PriceModule = require("../modules/PriceModule");
 
-describe("Price Module", () => {
+describe("Checking fuel price calculation", () => {
 
     test("In-state, has history, high volume", () => {
         const quote = new PriceModule("TX", true, 3000);
@@ -8,8 +8,10 @@ describe("Price Module", () => {
         expect(price).toEqual(5085);
     });
 
-    test.todo("out-state, no history, high volume");
-
-    test.todo("Out-state, has history, low volume");
+    test("Out-state, no history, low volume", () => {
+        const quote = new PriceModule("CA", false, 500);
+        let price = quote.getQuote()
+        expect(price).toEqual(877.5);
+    });
 
 })
