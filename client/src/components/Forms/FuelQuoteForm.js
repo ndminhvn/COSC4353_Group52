1e78ade63a9be408d0a7ad9d117a1da82f349259
Elinnoel Nuñez <elinnoelnunez@outlook.com>
2023-03-07 16:07:33 -0600

    cleanup, style changes

--- client/src/components/Forms/FuelQuoteForm.js
+++ client/src/components/Forms/FuelQuoteForm.js
@@ -6,25 +6,24 @@ import { getToken } from '../../utils/useToken.js';
 import './FuelQuoteForm.css'
 
 const testProfile = {
-    address1: '123 Main Street',
+    address: '123 Main Street',
     address2: '',
     city: 'Houston',
     state: 'TX',
-    zipcode: '77072',
+    zip: '77072',
 }
 
 const getMinDeliveryDays = () => {
     let curDate = new Date();
     const minDate = curDate.setDate(curDate.getDate() + 3);
-    return(new Date(minDate));
+    return (new Date(minDate));
 }
 
 const FuelQuoteForm = (props) => {
     const navigate = useNavigate();
-    // const username = localStorage.getItem('username');
+    const username = localStorage.getItem('username');
     // const token = getToken();
     const token = true;
-    const username = "randomUsername";
 
     const [gallons, setGallons] = useState('');
     const [selectedDate, setSelectedDate] = useState('');
@@ -38,16 +37,15 @@ const FuelQuoteForm = (props) => {
     //     deliveryDate: '',
     //     suggestedPPG: 0,
     //     totalDue: 0,
-    //     userId: 0,
     //     username: ""
     // })
 
     const [profile, setProfile] = useState({
-        address1: '',
+        address: '',
         address2: '',
         city: '',
         state: '',
-        zipcode: '',
+        zip: '',
     });
 
     const [quote, setQuote] = useState({
@@ -57,13 +55,9 @@ const FuelQuoteForm = (props) => {
         total: '0',
     });
 
-    const handleGallonsReq = (e) => {
-        setGallons(e.target.value);
-        // setData({ ...data, gallonsReq: gallons });
-    }
-
-    const handleSelectedDate = (e) => {
+    const onSelectedDate = (e) => {
         const day = new Date(e.target.value).getUTCDay();
+
         if ([6, 0].includes(day)) {
             e.preventDefault();
             e.target.value = '';
@@ -74,29 +68,12 @@ const FuelQuoteForm = (props) => {
         // setData({ ...data, deliveryDate: selectedDate });
     }
 
-    const handleGetQuote = async (e) => {
+    const onGetQuote = async (e) => {
 
         e.preventDefault();
 
-        if (quoteFormValid === true) {
-
-            // console.log("get quote gallons:", gallons)
-            // console.log("get quote selectedate:", selectedDate);
-
+        if (quoteFormValid) {
             // setData({ ...data, deliveryDate: selectedDate, gallonsReq: gallons });
-
-            // console.log("get quote data:", data)
-
-            // const options = {
-            //     method: 'GET',
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     body: JSON.stringify(value)
-            // };
-            // const response = await fetch(`${BASE_URL}/${username}/${gallons}`, options);
-            // const quote = await response.json();
-            // console.log(quote);
 
             // await axios.get(`${BASE_URL}/${username}/${gallons}`)
             // .then(res => {
@@ -104,15 +81,12 @@ const FuelQuoteForm = (props) => {
             // })
 
             // setData({
-            //     Gal_Req: gallons,
-            //     Del_Add: profile.address1,
-            //     Del_Dat: selectedDate,
-            //     Sug_Pri: quote.price,
-            //     Tot_Amo: quote.total,
-            //     // Users_Id: backendDetails.id,
-            //     // username: backendDetails.username
-            //     Users_id: 12345,
-            //     username: "username1"
+            //     gallonsReq: gallons,
+            //     deliveryAddress: profile.address,
+            //     deliveryDate: selectedDate,
+            //     suggestedPPG: quote.price,
+            //     totalDue: quote.total,
+            //     username: username
             // });
 
             setQuote({
@@ -126,7 +100,7 @@ const FuelQuoteForm = (props) => {
         }
     }
 
-    const handleQuoteSubmit = async (e) => {
+    const onQuoteSubmit = async (e) => {
         e.preventDefault();
 
         // probably redundant check due to having button disabled if invalid anyways
@@ -135,62 +109,28 @@ const FuelQuoteForm = (props) => {
         //     return;
         // }
 
-        // console.log("submit profile:", profile);
-        // console.log("submit quote:", quote);
-
         const mergedData = { ...profile, ...quote, username };
 
         console.log("submit mergedData:", mergedData);
     }
 
     useEffect(() => {
-        // console.log("token:", token);
 
         if (!token) {
             localStorage.clear();
             navigate('/login');
             window.location.reload(true);
         } else {
-            // console.log(BASE_URL);
-            // axios.get(`${BASE_URL}/account/${username}`, {
-            // headers: {
-            // Authorization: localStorage.getItem("token")
-            // }
-            // })
-            // .then(res => {
-            // setProfile(res.data);
-            // setProfile(prevProfile => testProfile);
-
             setProfile(testProfile);
 
-            // testing (possibly not necessary)
-            // setData(prevState => {
-            //     return {
-            //         ...prevState,
-            //         deliveryAddress: profile.address1 + "," + profile.city + "," + profile.state + "," + profile.zipcode
-            //     }
-            // });
-            // console.log("profile:",profile);
-            // console.log("data:", data)
-            // })
-            // .catch((err) => {
-            //     console.error(err);
-            //     alert("Something went wrong fetching profile data");
-            // });
-            console.log("gallons effect:", gallons);
-            console.log("selectedDate effect:", selectedDate);
-            console.log("quoteformvalid effect:", quoteFormValid);
-            // console.log("data:", data);
-
-            const handleCheckFormValid = () => {
+            const checkFormValidity = () => {
                 if (gallons !== '' && selectedDate !== '') {
                     setQuoteFormValid(true);
                 } else {
                     setQuoteFormValid(false);
                 }
             }
-
-            handleCheckFormValid();
+            checkFormValidity();
         }
     }, [token, gallons, selectedDate, quoteFormValid, quoteValid, navigate]);
 
@@ -202,7 +142,7 @@ const FuelQuoteForm = (props) => {
             </center>
             <div className='form-container'>
                 <div className='container'>
-                    <form onSubmit={handleGetQuote} style={{ 'padding': '10px' }}>
+                    <form onSubmit={onGetQuote} style={{ 'padding': '10px' }}>
                         <label className='label' htmlFor='requestedGallons' style={{ 'paddingTop': '0px' }}>
                             Gallons Requested
                         </label>
@@ -212,7 +152,7 @@ const FuelQuoteForm = (props) => {
                             name='requestedGallons'
                             placeholder='# of Gallons (100 min.)'
                             min='100'
-                            onChange={(e) => handleGallonsReq(e)}
+                            onChange={(e) => setGallons(e.target.value)}
                             required
                         />
                         <br />
@@ -224,7 +164,7 @@ const FuelQuoteForm = (props) => {
                             id='deliveryDate'
                             name='deliveryDate'
                             min={getMinDeliveryDays().toISOString().slice(0, 10)}
-                            onChange={handleSelectedDate}
+                            onChange={(e) => onSelectedDate(e)}
                             required
                         />
                         <label className='label' htmlFor='staticAddress'>
@@ -235,7 +175,7 @@ const FuelQuoteForm = (props) => {
                             id='staticAddress'
                             name='staticAddress'
                             placeholder='Address'
-                            value={profile.address1}
+                            value={profile.address}
                             readOnly
                             style={{ 'backgroundColor': '#d4d6d8b4' }}
                         />
@@ -274,19 +214,19 @@ const FuelQuoteForm = (props) => {
                             id='staticZip'
                             name='staticZip'
                             placeholder='ZIP Code'
-                            value={profile.zipcode}
+                            value={profile.zip}
                             readOnly
                             style={{ 'marginBottom': '10px', 'backgroundColor': '#d4d6d8b4' }}
                         />
                         <br />
-                        <button type='submit' disabled={!quoteFormValid} onSubmit={handleGetQuote}>
+                        <button type='submit' disabled={!quoteFormValid} onSubmit={onGetQuote}>
                             Request Quote
                         </button>
                         <br />
                     </form>
                 </div>
                 <div className='container'>
-                    <form onSubmit={handleQuoteSubmit} style={{ 'padding': '10px' }}>
+                    <form onSubmit={onQuoteSubmit} style={{ 'padding': '10px' }}>
                         <label className='label' htmlFor='staticGallons' style={{ 'padding': '0px', 'margin': '0px' }}>
                             Gallons Requested
                         </label>
@@ -298,7 +238,6 @@ const FuelQuoteForm = (props) => {
                             readOnly className='form-control-plaintext'
                             style={{ 'padding': '0px', 'margin': '0px', 'textAlign': 'center' }}
                         />
-
                         {quote.deliveryDate !== '' && <><label className='label' htmlFor='staticDate' style={{ 'padding': '0px', 'margin': '0px' }}>
                             Delivery Date
                         </label>
@@ -310,7 +249,6 @@ const FuelQuoteForm = (props) => {
                                 readOnly className='form-control-plaintext'
                                 style={{ 'padding': '0px', 'margin': '0px', 'textAlign': 'center' }}
                             /></>}
-
                         <label className='label' htmlFor='staticPricePerGallon' style={{ 'padding': '0px', 'margin': '0px' }}>
                             Suggested Price Per Gallon
                         </label>
@@ -324,7 +262,7 @@ const FuelQuoteForm = (props) => {
                         />
                         <label className='label' htmlFor='staticPriceDue' style={{ 'padding': '0px', 'margin': '0px' }}>
                             Estimated Price Due
-                        </label >
+                        </label>
                         <input
                             type='text'
                             id='staticPriceDue'
@@ -334,7 +272,7 @@ const FuelQuoteForm = (props) => {
                             readOnly className='form-control-plaintext'
                             style={{ 'padding': '0px', 'margin': '0px', 'textAlign': 'center' }}
                         />
-                        <button type='submit' disabled={!quoteValid} onSubmit={handleQuoteSubmit}>
+                        <button type='submit' disabled={!quoteValid} onSubmit={onQuoteSubmit}>
                             Submit Quote
                         </button>
                     </form>
