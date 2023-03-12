70d559cda47c73f081821026d459bb2f23f5a83e
Han Hoang <katiehoang268@gmail.com>
2023-03-11 21:29:38 -0600

    update server router

--- server/routes/Account.js
+++ server/routes/Account.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// View user profile
 router.get("/", (req, res) => {
-    res.send("Retrived profile Successfully")
+    res.status(200).json({
+        "message": "success"
+    })
 });
 
+// Create profile
 router.post("/", (req, res) => {
-    res.send("Update profile Successfully")
+    res.send("Created profile Successfully")
 });
+
+// Edit profile
+router.put("/", (req, res) => {
+    res.send("Updated profile Successfully")
+});
+
+module.exports = router;
 
 
 
