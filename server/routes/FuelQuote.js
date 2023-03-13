5cc2ee44213b4576822d468ab3ffb908e8aefeaa
Quang Du <qdtheta@gmail.com>
2023-03-12 19:56:43 -0500

    Finished quote post method

--- server/routes/FuelQuote.js
+++ server/routes/FuelQuote.js
@@ -9,18 +9,22 @@ router.get("/", async (req, res) => {
 
     const username = req.query.username;
     const gallons = req.query.gallons;
-    let state;
-    let hasHistory;
-    let unitCost;
-    let totalCost;
+
+    let hasHistory, unitCost, totalCost, fullAddress;
+    let address1, city, state, zipcode;
     
-    // Get client state
+    // Get client info
     try {
         let query = await pool.query(
-            `SELECT state 
+            `SELECT address1, city, state, zipcode 
             FROM users_info 
             WHERE username = '${username}'`);
+        
+        address1 = query.rows[0].address1;
+        city = query.rows[0].city;
         state = query.rows[0].state;
+        zipcode = query.rows[0].zipcode;
+        fullAddress = address1 + ", " + city + ", " + state + ", " + zipcode;
 
     } catch (error) {
         console.log(error)
@@ -40,28 +44,43 @@ router.get("/", async (req, res) => {
 
     // Get unit price and quote price
     const quote = new PriceModule(state,hasHistory,gallons);
-    unitCost = quote.getUnitCost()
-    totalCost = quote.getQuote()
+    unitCost = quote.getUnitCost();
+    totalCost = quote.getQuote();
 
     // Prepare JSON for front-end
     let body = {
         "gallons" : gallons, 
         "unitCost" : unitCost,
         "totalCost" : totalCost,
-        "inStateDiscount" : state == 'TX' ? true : false,
+        "deliveryAddress" : fullAddress,
+        "inStateDiscount" : state == "TX" ? true : false,
         "historyDiscount" : hasHistory}
 
     res.send(body);
          
 });
 
-router.post("/", (req, res) => {
+router.post("/", async(req, res) => {
 
-    // Handle dates
+    // Handle incomming data
     let orderDate = new Date().toISOString().slice(0, 10);
-    let purchaseDate = req.body.date;
+    const {username, deliveryDate, deliveryAddress, unitCost, gallons, totalCost} = req.body;
 
     // Save to database
+    try {
+
+        let query = await pool.query(
+            `INSERT INTO order_history 
+            (username, purchase_date, delivery_date, delivery_address, unit_cost, gallons_amount, total_cost)
+            VALUES 
+            ($1, $2, $3, $4, $5, $6, $7)`,
+            [username, orderDate, deliveryDate, deliveryAddress, unitCost, gallons, totalCost]);
+
+        console.log("Successfully save order to database")
+
+    } catch (error) {
+        console.log(error)
+    }
 
 });
 
