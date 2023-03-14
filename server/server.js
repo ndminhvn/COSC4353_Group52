f2ef6f97e63d38772be6db1c079e55913d9f2680
Du, Quang <73801104+theta1995@users.noreply.github.com>
2023-03-14 00:02:48 -0500

    Add more tests (#36)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const app = require("./app");
 const pool = require('./database/dbCreds');
-var bodyParser = require('body-parser')
 require("dotenv").config();
 
 // Connect to database
@@ -14,21 +11,6 @@ pool.connect((err) => {
 
 // Load dotenv
 const PORT = process.env.PORT || 6000;
-
-// CORS config
-app.use(cors());
-
-// Body-parser
-app.use(bodyParser.json())
-
-// Routes
-app.use("/", require("./routes/Index"));
-app.use("/login", require("./routes/Login"));
-app.use("/register", require("./routes/Register"));
-app.use("/history", require("./routes/FuelHistory"));
-app.use("/account", require("./routes/Account")); 
-app.use("/quote", require("./routes/FuelQuote"));
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
