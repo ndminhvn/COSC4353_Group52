78334d55b3fb96a8eca78ebfbefe04608ba86ec3
Quang Du <qdtheta@gmail.com>
2023-03-15 20:10:41 -0500

    Update Unit Test

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -25,12 +25,19 @@ describe("Fuel Quote Route", () => {
         expect(response.statusCode).toBe(200);
     });
 
-    test("POST/ Bad data received", async () => {
-        const response = await request(app).post("/quote/").send({
-            badKey: "jimmy",
-            missingKey: "333"
-        }) 
+    test("GET/ Valid client with no profile data", async () => {
+        const response = await request(app).get("/quote/?username=tom&gallons=300");
+        expect(response.statusCode).toBe(403);
+    });
+
+    test("GET/ Invalid URL query", async () => {
+        const response = await request(app).get("/quote/?dfgdfg=tom&gallons=300");
         expect(response.statusCode).toBe(400);
-    })
+    });
+
+    test("GET/ username does not exist", async () => {
+        const response = await request(app).get("/quote/?username=sadsadad&gallons=300");
+        expect(response.statusCode).toBe(500);
+    });
 
 })
